dcf9ea9fd9374011b44239971af7fb27d3094d7a
Abraham Lopez <abraham_lopez@live.com>
2021-03-13 14:31:42 -0500

    Created a reusable GAPSIAlertVC that is going to help us show an error message in case something goes wrong with the Network call or if the user did not entered any search parameter and is trying to hit Search Products Button.

---
+++ GAPSI/CustomViews/GAPSIAlertVC.swift
@@ -0,0 +1,104 @@
+//
+//  GAPSIAlertVC.swift
+//  GAPSI
+//
+//  Created by Abraham on 3/13/21.
+//
+
+import UIKit
+
+class GAPSIAlertVC: UIViewController {
+
+    private let containerView   = GAPSIAlertContainerView()
+    private let titleLabel      = GAPSITitleLabel(textAlignment: .center, fontSize: 20)
+    private let messageLabel    = GAPSIBodyLabel(textAlignment: .center)
+    private let actionButton    = GAPSIButton(backgroundColor: .systemPink, title: "Ok")
+    
+    private var alertTitle: String?
+    private var message: String?
+    private var buttonTitle: String?
+    
+    private let padding: CGFloat = 20
+    
+    
+    init(title: String, message: String, buttonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle     = title
+        self.message        = message
+        self.buttonTitle    = buttonTitle
+    }
+    
+    // This method is necessary because I am not using Storyboards
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
+        view.addSubview(containerView)
+        view.addSubview(titleLabel)
+        view.addSubview(actionButton)
+        view.addSubview(messageLabel)
+        
+        configureContainerView()
+        configureTitleLabel()
+        configureActionButton()
+        configureMessageLabel()
+    }
+    
+    // Configure NSLayoutConstraints for the containerView
+    private func configureContainerView() {
+        NSLayoutConstraint.activate([
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 280),
+            containerView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+    }
+    
+    // Configure NSLayoutConstraints for the titleLabel
+    private func configureTitleLabel() {
+        titleLabel.text = alertTitle ?? "Oops Something went wrong!"
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: padding),
+            titleLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            titleLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            titleLabel.heightAnchor.constraint(equalToConstant: 28)
+        ])
+    }
+    
+    // Configure NSLayoutConstraint for the actionButton
+    private func configureActionButton() {
+        actionButton.setTitle(buttonTitle ?? "Ok", for: .normal)
+        actionButton.addTarget(self, action: #selector(dismissAlertVC), for: .touchUpInside)
+        
+        NSLayoutConstraint.activate([
+            actionButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+    
+    // Configure NSLayoutConstraints for the messageLabel
+    private func configureMessageLabel() {
+        messageLabel.text           = message ?? "Unable to complete search request."
+        messageLabel.numberOfLines  = 4
+        
+        NSLayoutConstraint.activate([
+            messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 8),
+            messageLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: padding),
+            messageLabel.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -padding),
+            messageLabel.bottomAnchor.constraint(equalTo: actionButton.topAnchor, constant: -12)
+        ])
+    }
+    
+    // This method would let us dismiss the GAPSIAlertVC
+    @objc func dismissAlertVC() {
+        dismiss(animated: true)
+    }
+
+}

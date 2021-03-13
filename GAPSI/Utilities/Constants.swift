f006351a794d6441fd31fc27d3fc17edf109ac9e
Abraham Lopez <abraham_lopez@live.com>
2021-03-13 14:25:51 -0500

    Created a Constants utility file for our constants, in this case the cartLogoImage for the main screen.

---
+++ GAPSI/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GAPSI
+//
+//  Created by Abraham on 3/13/21.
+//
+
+import UIKit
+
+enum Images {
+    static let eCommerceLogo                   = UIImage(named: "cartLogo")
+}

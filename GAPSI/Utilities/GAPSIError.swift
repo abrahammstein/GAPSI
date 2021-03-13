6d7a329657877c83466bfea71bdcc52caa705ddb
Abraham Lopez <abraham_lopez@live.com>
2021-03-13 17:16:27 -0500

    Added 2 more cases to the GAPSIError in order to show the user if the searched criteria was already saved or not.

--- GAPSI/Utilities/GAPSIError.swift
+++ GAPSI/Utilities/GAPSIError.swift
@@ -13,4 +13,6 @@ enum GAPSIError: String, Error {
     case unableToComplete   = "Unable to complete your request. Please check your internet connection"
     case invalidResponse    = "Invalid response from the server. Please try again."
     case invalidData        = "The data received from the server was invalid. Please try again."
+    case alreadyInHistory = "You've already searched for this product. You must REALLY like it! 😍."
+    case unableToSaveToHistory   = "There was an error saving this product to the history. Please try again."
 }
